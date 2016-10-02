b7b0cf4521e3d48f51f83685446fa86e00dd7499
oberwelle <welle@oberwelle.de>
2016-10-02 14:02:06 +0200

    Webview added

--- app/src/main/java/de/oberwelle/rasen/MainActivity.java
+++ app/src/main/java/de/oberwelle/rasen/MainActivity.java
@@ -2,12 +2,24 @@ package de.oberwelle.rasen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setDisplayZoomControls(false);
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("http://192.168.2.106/");
     }
 }
